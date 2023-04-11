3adca950f00b9506b77b0a095bf053a1793cf4e6
maxvanbart <maxvanbart@gmail.com>
2023-04-11 12:01:32 +0200

    disjoint index groupes

--- src/algo1.py
+++ src/algo1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def algo1(gradients):
@@ -22,8 +23,28 @@ def algo1(gradients):
     for c in range(K):
         r.append(G_H[c, :] / G_H1)
 
-    r_unique = np.unique(r[1], return_counts=True)
-    r = np.array(r)
+    # Make the disjoint index groupes for all the values occuring multiple times in r2
+    r2 = r[1]
+    r2_unique = np.unique(r2, return_counts=True)
+    unique_vals = []
+    I = []
+    for i in range(r2_unique[0].shape[0]):
+        if r2_unique[1][i] > 1:
+            print(f"Value {r2_unique[0][i]} has {r2_unique[1][i]} occurences.")
+            unique_vals.append(r2_unique[0][i])
+    for val in unique_vals:
+        I.append([])
+        for i in range(r2.shape[0]):
+            if not math.isnan(val):
+                if r2[i] == val:
+                    I[-1].append(i)
+            else:
+                if math.isnan(r2[i]):
+                    I[-1].append(i)
+
+    # exclude the group of disjoint indices which is made by the NaN values
+    I = I[:-1]
+
 
     pass
     pass
