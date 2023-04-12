7f7cf58e958f7540b1ad0d9bef6fdfc9fb6dd84f
HugoChass <62014029+HugoChass@users.noreply.github.com>
2023-04-12 19:02:04 +0200

    added algo2

--- src/algo2.py
+++ src/algo2.py
@@ -1,4 +1,8 @@
+import numpy
 
-
-def algo2(g_mc):
-    pass
+def algo2(ratio):
+    lst = [x for x in ratio if x < 0]
+    if len(lst) == 1:
+        return numpy.where(ratio == lst[0])
+    else:
+        return 0
