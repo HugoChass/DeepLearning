0f1b5719a916633a8f4a60f2410e9184eb7432bb
maxvanbart <maxvanbart@gmail.com>
2023-04-26 17:30:49 +0200

    formatting

--- src/algo3.py
+++ src/algo3.py
@@ -17,7 +17,7 @@ def algo3(gradients, I_mH, g_mc):
         for m in range(M):
             j = random.choice(I_cur[m])
             ind = 0
-            #print(G[i][:,j])
+            # print(G[i][:,j])
             for val in G[i][:, j]:  # check for the zero's in the col, if so, not activated in diag
                 if val != 0:
                     D[m][i][ind, ind] = 1
@@ -26,14 +26,14 @@ def algo3(gradients, I_mH, g_mc):
         # Create temporary list for Exans sets
         I_sets_temp = []
         # Loop over the D maxtrices create for each sample at a certain layer
-        for d in D[:,i]:
-            I_set_temp = [] # Create temporary list for index set for a sample
+        for d in D[:, i]:
+            I_set_temp = []  # Create temporary list for index set for a sample
             for n in range(d.shape[0]):
-                if d[n,n] == 1: # If the diagonal value is 1 then it is an exan and should be added to the set
+                if d[n, n] == 1:  # If the diagonal value is 1 then it is an exan and should be added to the set
                     I_set_temp.append(n)
-            I_sets_temp.append(I_set_temp) # Append smaple index set
-        
-        I_cur = I_sets_temp # Update I_cur
+            I_sets_temp.append(I_set_temp)  # Append smaple index set
+
+        I_cur = I_sets_temp  # Update I_cur
 
 
 
